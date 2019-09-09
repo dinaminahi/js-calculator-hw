8a6e42e0c4f0edeb250c5f802fb7803f9ca0db20
dinaminahi <diankagrechukh@gmail.com>
2019-09-09 12:27:30 +0000

    update calculator

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const nums = document.querySelectorAll('.digits button');
 const opers = document.querySelectorAll('.opers button');
 let display = document.querySelector('.display');
 const equal = document.querySelector('.equal');
+const dot = document.querySelector('.dot');
+const clear = document.querySelector('.clear');
 
 nums.forEach(num => num.addEventListener('click', digitPressed));
 
@@ -13,26 +15,31 @@ opers.forEach(oper => oper.addEventListener('click', operPressed));
 
 function operPressed (ev) {
     const oper = ev.target.innerText;
-
         if(!display.value.includes('-') && !display.value.includes('+') && !display.value.includes('/') && !display.value.includes('*')){
             display.value += oper;
-        }else{
+        }else if(display.value.charAt(display.value.length - 1) === '-' || display.value.charAt(display.value.length - 1) === '+' || display.value.charAt(display.value.length - 1) === '*' || display.value.charAt(display.value.length - 1) === '/' ){
            let text = display.value.split('');
            text.pop();
-            text.push(oper);
-            display.value = text.join('');
-
+           text.push(oper);
+           display.value = text.join('');
         }
-
-
 }
+
  equal.addEventListener('click', quantify);
 
  function quantify() {
-
-    display.value = eval(display.value);
-    //  const text = eval(display.value);
-    //   if(!(text === Number.POSITIVE_INFINITY)){
-    //      display.value = text;
-    //   }
+     const text = eval(display.value);
+      if(!(text === Number.POSITIVE_INFINITY)){
+         display.value = text;
+      }else{
+          display.value = 'Error';
+      }
  }
+
+dot.addEventListener('click', dotPressed);
+
+function dotPressed (ev) {
+    if(!display.value.includes(ev.target.innerText)){
+       display.value += ev.target.innerText;
+    }
+}
