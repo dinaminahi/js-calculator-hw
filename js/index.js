ed474ab55b8ce7772dc56a057886d651794e4545
dinaminahi <diankagrechukh@gmail.com>
2019-09-09 10:08:45 +0000

    update js file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const nums = document.querySelectorAll('.digits button');
 const opers = document.querySelectorAll('.opers button');
-const display = document.querySelector('.display');
+let display = document.querySelector('.display');
 const equal = document.querySelector('.equal');
 
 nums.forEach(num => num.addEventListener('click', digitPressed));
@@ -16,14 +16,23 @@ function operPressed (ev) {
 
         if(!display.value.includes('-') && !display.value.includes('+') && !display.value.includes('/') && !display.value.includes('*')){
             display.value += oper;
+        }else{
+           let text = display.value.split('');
+           text.pop();
+            text.push(oper);
+            display.value = text.join('');
+
         }
+
 
 }
  equal.addEventListener('click', quantify);
 
  function quantify() {
-     const text = eval(display.value);
-      if(!(text === Number.POSITIVE_INFINITY)){
-         display.value = text;
-      }
+
+    display.value = eval(display.value);
+    //  const text = eval(display.value);
+    //   if(!(text === Number.POSITIVE_INFINITY)){
+    //      display.value = text;
+    //   }
  }
